cfe824ab3911dcfb8091817806f5afe0d25bc256
IfanBi <50548381+IfanBi@users.noreply.github.com>
2019-11-28 14:36:38 +0700

    Update index.php
    Mengubah tittle menjadi seperti semula

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -9,7 +9,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Kos Bintang Telang Indah</title>
+  <title>Kos Bintang Telang</title>
 
   <!-- Custom fonts for this template-->
   <link href="<?= base_url('assets/')?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
