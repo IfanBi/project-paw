293b05b7a055a6c8f49208ebcd05d888dd99dfc8
LuqmanHkim <57902751+haxienew@users.noreply.github.com>
2019-11-28 23:04:02 +0700

    Update index.php
    homepage rebuild

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-  <title>Cargo &mdash; Website Template by Colorlib</title>
+  <title>Homepage - Kos Bintang Telang</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -185,53 +185,6 @@
           </div>
         </div>
 
-      </div>
-
-
-
-      <div class="site-section bg-light" id="about-section">
-        <div class="container">
-          <div class="row justify-content-center mb-4 block-img-video-1-wrap">
-            <div class="col-md-12 mb-5">
-              <figure class="block-img-video-1" data-aos="fade">
-                <a href="https://vimeo.com/45830194" data-fancybox data-ratio="2">
-                  <span class="icon"><span class="icon-play"></span></span>
-                  <img src="images/cargo_delivery_big.jpg" alt="Image" class="img-fluid">
-                </a>
-              </figure>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-12">
-              <div class="row">
-                <div class="col-6 col-md-6 mb-4 col-lg-0 col-lg-3" data-aos="fade-up" data-aos-delay="">
-                  <div class="block-counter-1">
-                    <span class="number"><span data-number="50">0</span>+</span>
-                    <span class="caption">Years of Experience</span>
-                  </div>
-                </div>
-                <div class="col-6 col-md-6 mb-4 col-lg-0 col-lg-3" data-aos="fade-up" data-aos-delay="100">
-                  <div class="block-counter-1">
-                    <span class="number"><span data-number="300">0</span>+</span>
-                    <span class="caption">Companies</span>
-                  </div>
-                </div>
-                <div class="col-6 col-md-6 mb-4 col-lg-0 col-lg-3" data-aos="fade-up" data-aos-delay="200">
-                  <div class="block-counter-1">
-                    <span class="number"><span data-number="108">0</span>+</span>
-                    <span class="caption">Covered Countries</span>
-                  </div>
-                </div>
-                <div class="col-6 col-md-6 mb-4 col-lg-0 col-lg-3" data-aos="fade-up" data-aos-delay="300">
-                  <div class="block-counter-1">
-                    <span class="number"><span data-number="1500">0</span>+</span>
-                    <span class="caption">Couriers</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
 
       <div class="site-section" id="team-section">
