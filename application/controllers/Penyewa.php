25706360df2e103a05c2ca4a1376ed7e4d508705
LuqmanHkim <57902751+haxienew@users.noreply.github.com>
2019-12-02 20:31:31 +0700

    Resetpass User
    Menambahkan fitur mengganti password untuk user

--- application/controllers/Penyewa.php
+++ application/controllers/Penyewa.php
@@ -48,4 +48,32 @@ class Penyewa extends CI_Controller
 		);
 		$this->load->view('penyewa/daftarkamar', $data);
 	}
+
+	public function resetpass($username)
+	{
+		$data['useractive'] = $this->db->get_where('tbl_penyewa', ['username'=>$this->session->userdata('username')])->row_array();
+		$data['akun'] = $this->db->get_where('tbl_akun', ['username'=>$username])->row_array();
+
+		if (isset($_POST['submit'])) {
+			$this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]',['matches' => 'Password tidak sama', 'min_length' => 'Password terlalu pendek']);
+			$this->form_validation->set_rules('password2', 'Password', 'required|trim|min_length[3]|matches[password1]');
+			if ($this->form_validation->run() == false) 
+			{
+				$this->load->view('penyewa/resetpass', $data);
+			}else{
+				$up = array(
+					'username' => $username,
+					'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+					'level' => $this->input->post('level')
+				);
+				$this->db->where('username', $username);
+				$this->db->update('tbl_akun', $up);
+
+				$this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Password Berhasil diubah!</div>');
+				redirect('penyewa/');
+			}
+		}else{
+			$this->load->view('penyewa/resetpass', $data);
+		}
+	}
 }
