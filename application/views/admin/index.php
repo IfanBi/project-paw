c6133911c5891e20e8e4531281a92b6ebd19ff36
LuqmanHkim <57902751+haxienew@users.noreply.github.com>
2019-12-02 20:11:32 +0700

    Menu Changepass admin
    mengubah url menuju resetpass/admin

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -97,7 +97,7 @@
                   <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                   Profile
                 </a>
-                <a class="dropdown-item" href="<?= base_url('admin/'); ?>">
+                <a class="dropdown-item" href="<?= base_url('admin/resetpass/'.$useractive['username']); ?>">
                   <i class="fas fa-edit fa-sm fa-fw mr-2 text-gray-400"></i>
                   Ganti Password
                 </a>
